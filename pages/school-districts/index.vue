2df5a7a008754aadfa9eaeee7031af992fa86117
kodx <adil@kodxsystem.com>
2024-03-22 11:25:36 +0500

    school district pagination added & code optimize

--- pages/school-districts/index.vue
+++ pages/school-districts/index.vue
@@ -12,6 +12,48 @@ import {
 import { XMarkIcon } from "@heroicons/vue/24/outline";
 import FilterLines from "~/assets/icons/filterLines.svg";
 import SearchIcon from "~/assets/icons/search-icon.svg";
+import BriefCase from "~/assets/icons/brief-case.svg";
+import GraduationHat from "~/assets/icons/graduation-hat.svg";
+import Building from "~/assets/icons/building.svg";
+
+const jobOptions = ref([
+  { id: "1", label: "0 to 10", checked: false },
+  { id: "2", label: "11 to 50", checked: true },
+  { id: "3", label: "51 to 100", checked: false },
+  { id: "4", label: "100+", checked: false },
+]);
+
+const stuOptions = ref([
+  { id: "1", label: "0 to 100", checked: false },
+  { id: "2", label: "101 to 500", checked: false },
+  { id: "3", label: "501 to 1000", checked: true },
+  { id: "4", label: "1001 to 2000", checked: false },
+  { id: "4", label: "2001 to 3000", checked: false },
+  { id: "4", label: "3001 to 5000", checked: false },
+  { id: "4", label: "5001 to 10,000", checked: false },
+  { id: "4", label: "10,000+", checked: false },
+]);
+
+const schOptions = ref([
+  { id: "1", label: "0 to 10", checked: false },
+  { id: "2", label: "11 to 25", checked: true },
+  { id: "3", label: "26 to 50", checked: false },
+  { id: "4", label: "51 to 100", checked: false },
+]);
+
+const clearAll = () => {
+  jobOptions?.value?.forEach((option: any) => {
+    option.checked = false;
+  });
+
+  stuOptions?.value?.forEach((option: any) => {
+    option.checked = false;
+  });
+
+  schOptions?.value?.forEach((option: any) => {
+    option.checked = false;
+  });
+};
 
 const capitals = ref<string[]>([]); // Declare capitals as a ref of type string array
 for (let i = 65; i <= 90; i++) {
@@ -145,7 +187,53 @@ const selectAlphabet = (index: number) => {
                   </button>
                 </div>
               </TransitionChild>
-              <DistrictsFilters />
+              <div
+                class="py-2 flex-col justify-start items-start gap-2.5 inline-flex"
+              >
+                <div
+                  class="justify-between items-center inline-flex w-full border-b border-gray-200 pb-2"
+                >
+                  <div class="justify-start items-center gap-3 flex">
+                    <FilterLines class="size-6" />
+                    <div
+                      class="text-gray-700 text-base font-semibold leading-normal"
+                    >
+                      Filters
+                    </div>
+                  </div>
+                  <div class="justify-center items-center gap-1.5 flex">
+                    <button
+                      @click="clearAll"
+                      class="text-brand-800 text-sm font-semibold leading-tight"
+                    >
+                      Clear All
+                    </button>
+                  </div>
+                </div>
+
+                <div class="divide-y divide-gray-200">
+                  <FilterSection
+                    title="No. of Jobs"
+                    :options="jobOptions"
+                    :icon-component="BriefCase"
+                    total-jobs="125"
+                  />
+
+                  <FilterSection
+                    title="No. of students"
+                    :options="stuOptions"
+                    :icon-component="GraduationHat"
+                    total-jobs="12,000"
+                  />
+
+                  <FilterSection
+                    title="No. of schools"
+                    :options="schOptions"
+                    :icon-component="Building"
+                    total-jobs="13"
+                  />
+                </div>
+              </div>
             </DialogPanel>
           </TransitionChild>
         </div>
@@ -157,7 +245,51 @@ const selectAlphabet = (index: number) => {
       <div
         class="flex grow flex-col gap-y-5 pt-8 overflow-y-auto border-r pr-4 border-gray-200 bg-transparent ring-1 ring-white/5"
       >
-        <DistrictsFilters />
+        <div
+          class="py-2 flex-col justify-start items-start gap-2.5 inline-flex"
+        >
+          <div
+            class="justify-between items-center inline-flex w-full border-b border-gray-200 pb-2"
+          >
+            <div class="justify-start items-center gap-3 flex">
+              <FilterLines class="size-6" />
+              <div class="text-gray-700 text-base font-semibold leading-normal">
+                Filters
+              </div>
+            </div>
+            <div class="justify-center items-center gap-1.5 flex">
+              <button
+                @click="clearAll"
+                class="text-brand-800 text-sm font-semibold leading-tight"
+              >
+                Clear All
+              </button>
+            </div>
+          </div>
+
+          <div class="divide-y divide-gray-200">
+            <FilterSection
+              title="No. of Jobs"
+              :options="jobOptions"
+              :icon-component="BriefCase"
+              total-jobs="125"
+            />
+
+            <FilterSection
+              title="No. of students"
+              :options="stuOptions"
+              :icon-component="GraduationHat"
+              total-jobs="12,000"
+            />
+
+            <FilterSection
+              title="No. of schools"
+              :options="schOptions"
+              :icon-component="Building"
+              total-jobs="13"
+            />
+          </div>
+        </div>
       </div>
     </div>
 
@@ -172,24 +304,23 @@ const selectAlphabet = (index: number) => {
       </div>
 
       <div class="pt-8 flex sm:flex-row flex-col gap-4 justify-between">
-        <form class="flex flex-1" action="#" method="GET">
-          <label for="search-field" class="sr-only">Search</label>
-          <div class="relative">
-            <SearchIcon
-              class="pointer-events-none absolute inset-y-0 left-2 h-full w-5 text-gray-500"
-              aria-hidden="true"
-            />
-            <input
-              id="search-field"
-              class="block h-full w-[320px] shadow border border-gray-300 bg-transparent py-[13px] pl-8 pr-0 text-black focus:ring-0 sm:text-sm"
-              placeholder="Search..."
-              type="search"
-              name="search"
-            />
-          </div>
-        </form>
-
-        <div class="inline-flex">
+        <div class="flex">
+          <form class="flex flex-1" action="#" method="GET">
+            <label for="search-field" class="sr-only">Search</label>
+            <div class="relative">
+              <SearchIcon
+                class="pointer-events-none absolute inset-y-0 left-2 h-full w-5 text-gray-500"
+                aria-hidden="true"
+              />
+              <input
+                id="search-field"
+                class="block h-full w-[320px] shadow border border-gray-300 bg-transparent py-[13px] pl-8 pr-0 text-black focus:ring-0 sm:text-sm"
+                placeholder="Search..."
+                type="search"
+                name="search"
+              />
+            </div>
+          </form>
           <div class="flex shrink-0 items-center shadow-sm pr-3">
             <button
               type="button"
@@ -200,7 +331,9 @@ const selectAlphabet = (index: number) => {
               <FilterLines class="size-6" />
             </button>
           </div>
+        </div>
 
+        <div class="inline-flex justify-end">
           <div class="rounded-lg shadow justify-start items-start inline-flex">
             <button
               type="button"
@@ -236,7 +369,9 @@ const selectAlphabet = (index: number) => {
         </div>
       </div>
 
-      <div class="pt-6 w-full gap-2 flex flex-col xl:flex-row">
+      <div
+        class="pt-6 w-full gap-2 flex flex-col xl:flex-row border-b border-gray-200"
+      >
         <div>
           <p class="text-gray-500 text-sm font-semibold !w-[139px]">
             Search by alphabet
@@ -246,16 +381,20 @@ const selectAlphabet = (index: number) => {
         <div
           class="flex flex-wrap gap-2.5 sm:gap-x-0 items-center w-full justify-between"
         >
-          <div v-for="(capital, index) in capitals" :key="index" class="pr-1.5">
+          <div
+            v-for="(capital, index) in capitals"
+            :key="index"
+            class="pr-1.5 borer-b border-gray-200"
+          >
             <button
               :class="[
                 index === selectedAlphabet
-                  ? 'text-blue-800 border-b-2 border-blue-800'
+                  ? 'text-blue-800 border-b-2 px-[5px] border-blue-800'
                   : '',
               ]"
               @click="selectAlphabet(index)"
             >
-              {{ capital }}
+              <span class="text-xs md:text-sm">{{ capital }}</span>
             </button>
           </div>
           <div class="text-brand-800 text-sm font-semibold leading-tight">
@@ -264,7 +403,7 @@ const selectAlphabet = (index: number) => {
         </div>
       </div>
 
-      <div class="border-t mt-1.5 mb-8 border-gray-200">
+      <div class="mt-1.5 mb-8">
         <!-- Grid View -->
         <div
           v-if="isGridView"
@@ -281,6 +420,8 @@ const selectAlphabet = (index: number) => {
           </div>
         </div>
       </div>
+
+      <Pagination />
     </div>
   </div>
 </template>
