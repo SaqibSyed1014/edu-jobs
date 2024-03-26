56a034ae0ec97d948d0776d359ebd3ca89a43db7
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-03-27 01:00:35 +0500

    Job descriptions hidden

--- components/pages/home/FeaturedJobs.vue
+++ components/pages/home/FeaturedJobs.vue
@@ -117,7 +117,7 @@ const sliderBreakpoints = {
                 class="jobs-slider z-20"
             >
               <swiper-slide v-for="job in jobPostsList" class="md:!w-[340px]">
-                <JobCard :job="job" :card-form="true" :show-job-description="true" />
+                <JobCard :job="job" :card-form="true" :show-job-description="false" />
               </swiper-slide>
             </swiper-container>
 
