866987b08432f9ddce8b598535e35a98a8d03c1f
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-04-05 03:31:13 +0500

    Blog API integrated in Home and Blog pages

--- types/blogs.types.ts
+++ types/blogs.types.ts
@@ -4,16 +4,22 @@ interface Blog {
     author: AuthorDetail
     category: Category
     post_content: string
+    post_excerpt: string
     post_date: string
     publishedAt: Date
     updatedAt: Date
     createdAt: Date
     slug: string
+    post_photo: {
+        id: string
+        url: string
+    }
 }
 
 interface AuthorDetail {
     id: number
     name: string
+    avatar: string
 }
 
 interface Category {
