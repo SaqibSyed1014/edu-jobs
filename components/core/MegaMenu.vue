6f9d4bc2c563ad9f6749b0aceae3abbc88d3314e
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-04-09 17:42:16 +0500

    mega menu arrow fix

--- components/core/MegaMenu.vue
+++ components/core/MegaMenu.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="mega-menu relative inline-block" @mouseenter="showMenu = true" @mouseleave="showMenu = false">
-    <div class="mega-menu-trigger flex items-center gap-6 relative cursor-pointer" :class="[{ 'before:hidden after:hidden': !showMenu }]">
+  <div class="mega-menu relative inline-block py-3" @mouseenter="showMenu = true" @mouseleave="showMenu = false">
+    <div class="mega-menu-trigger flex items-center gap-4 relative cursor-pointer" :class="[{ 'before:opacity-0 after:opacity-0': !showMenu }]">
       {{ label }}
       <div class="text-gray-600">
         <SvgoChevronDown class="w-5 h-5 transition" :class="{ 'rotate-180': showMenu }" />
@@ -9,7 +9,7 @@
     <Transition name="zoom-in">
       <div
           v-if="showMenu"
-          class="menu absolute top-full border border-gray-200 mt-7 left-1/3 -translate-x-1/3 z-[50] bg-white rounded-lg shadow-lg p-6"
+          class="menu absolute top-full border border-gray-200 mt-2.5 left-1/3 -translate-x-1/3 z-[50] bg-white rounded-lg shadow-lg p-6"
       >
         <slot />
       </div>
@@ -29,7 +29,7 @@ const props = defineProps<{
 .mega-menu-trigger:before {
   content: "";
   position: absolute;
-  top: 43px;
+  top: 35px;
   left: 50%;
   transform: translate(-50%);
   width: 0;
@@ -38,11 +38,12 @@ const props = defineProps<{
   border-width: 0 15px 15px 15px;
   border-color: transparent transparent #ffffff transparent;
   z-index: 111;
+  transition: opacity 0.3s ease;
 }
 .mega-menu-trigger:after {
   content: "";
   position: absolute;
-  top: 41.5px;
+  top: 33.5px;
   left: 50%;
   transform: translate(-50%);
   width: 0;
@@ -51,6 +52,7 @@ const props = defineProps<{
   border-width: 0 15px 15px 15px;
   border-color: transparent transparent theme('colors.gray.300') transparent;
   z-index: 11;
+  transition: opacity 0.3s ease;
 }
 
 .zoom-in-enter-active,
