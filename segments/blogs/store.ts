2feeedb30fb3f3658ac09e00a12c6a8803c58ede
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-06-17 18:34:42 +0500

    fetch blogs by categories integrated

--- segments/blogs/store.ts
+++ segments/blogs/store.ts
@@ -1,9 +1,11 @@
 import {
     getBlogsList,
+    getBlogsByCategory,
     getBlogsCategories,
     getBlogDetails
 } from "~/segments/blogs/services";
 import type {PaginationInfo} from "~/segments/common.types";
+import type {DropdownOptions} from "flowbite";
 
 interface BlogState {
     blogs: Blog[]
@@ -41,6 +43,17 @@ export const useBlogStore = defineStore('blogStore', {
                     throw err;
                 })
         },
+        async fetchBlogsByCategory(pageInfo :PaginationInfo, category :string) {
+            return await getBlogsByCategory(pageInfo.currentPage, pageInfo.itemsPerPage, category)
+                .then(({ data, meta }) => {
+                    this.$state.blogs = data;
+                    this.$state.pagination = meta.pagination
+                })
+                .catch((err) => {
+                    console.log('error ', err);
+                    throw err;
+                })
+        },
         async fetchBlogsCategories() {
             return await getBlogsCategories()
                 .then(({data, meta }) => {
@@ -61,5 +74,17 @@ export const useBlogStore = defineStore('blogStore', {
                     throw err;
                 })
         }
+    },
+    getters: {
+        categoriesDropdown: (state) :{ label: string, value: number }[] => {
+            const categoriesArray = state.categories.map((category: BlogCategory) => ({
+                label: category.category_name,
+                value: category.id
+            }));
+
+            categoriesArray.unshift({ label: 'View All', value: -1 });
+
+            return categoriesArray;
+        },
     }
 })
