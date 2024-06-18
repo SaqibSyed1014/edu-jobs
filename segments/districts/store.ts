462d7859d8b0ecbce5448a0e8797052c177946e9
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-06-19 02:08:44 +0500

    districts filter by alphabet

--- segments/districts/store.ts
+++ segments/districts/store.ts
@@ -20,8 +20,6 @@ export const useDisrictsStore = defineStore('districtStore', {
     } as DistrictState),
     actions: {
         async fetchDistricts(query:any) {
-            if (query.filter_by) query.filter_by = 'status:=\'active\'&&' + query.filter_by
-            else query.filter_by = 'status:=\'active\''
             const { hits, found} = await getDistrictList(query)
             // const response = await useGet(`/collections/districts/documents/search?q=*&per_page=10`)
             this.$state.distictsList = hits
