368f800a955d4e725eb6ece4088267dd2485f016
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-06-25 18:54:38 +0500

    list of colleges tab removed + schoo list, video and images removed from detail page

--- pages/colleges/[id]/index.vue
+++ pages/colleges/[id]/index.vue
@@ -9,8 +9,7 @@ const activeTab = ref(0); // Default to first tab
 // Array of tab names and icons
 const tabs = ref([
   { name: "About College District" },
-  { name: "Open Jobs" },
-  { name: "List of Colleges" }
+  { name: "Open Jobs" }
 ]);
 
 const listData = ref([
@@ -33,124 +32,6 @@ const listData = ref([
   {
     title: "Challenges:",
     desc: "Like many high-performing school districts, PAUSD faces challenges related to academic pressure, student well-being, and maintaining a balance between achievement and the overall well-rounded development of students.",
-  },
-]);
-
-const schoolList = ref([
-  {
-    avatar: "/images/schoolDistrict/Avatar1.png",
-    schoolName: "Palo Alto Unified College District",
-  },
-  {
-    avatar: "/images/schoolDistrict/avatar2.png",
-    schoolName: "San Dieguito Union High College District",
-  },
-  {
-    avatar: "/images/schoolDistrict/avatar3.png",
-    schoolName: "Arcadia Unified College District",
-  },
-  {
-    avatar: "/images/schoolDistrict/avatar4.png",
-    schoolName: "Saratoga Joint Union High College District",
-  },
-  {
-    avatar: "/images/schoolDistrict/avatar5.png",
-    schoolName: "Palo Alto Unified College Districts",
-  },
-  {
-    avatar: "/images/schoolDistrict/avatar6.png",
-    schoolName: "San Dieguito Union High College District",
-  },
-  {
-    avatar: "/images/schoolDistrict/avatar7.png",
-    schoolName: "Arcadia Unified College District",
-  },
-  {
-    avatar: "/images/schoolDistrict/avatar8.png",
-    schoolName: "Saratoga Joint Union High College District",
-  },
-
-  {
-    avatar: "/images/schoolDistrict/avatar9.png",
-    schoolName: "Palo Alto Unified College Districts",
-  },
-  {
-    avatar: "/images/schoolDistrict/avatar10.png",
-    schoolName: "San Dieguito Union High College District",
-  },
-  {
-    avatar: "/images/schoolDistrict/avatar11.png",
-    schoolName: "Arcadia Unified College District",
-  },
-  {
-    avatar: "/images/schoolDistrict/avatar12.png",
-    schoolName: "Saratoga Joint Union High College District",
-  },
-]);
-
-const photoList = ref([
-  {
-    image: "/images/schoolDistrict/photo1.jpg",
-    title: "Practical education",
-    date: "20 Jan 2024",
-  },
-  {
-    image: "/images/schoolDistrict/photo2.jpg",
-    title: "Practical education",
-    date: "20 Jan 2024",
-  },
-  {
-    image: "/images/schoolDistrict/photo3.jpg",
-    title: "Practical education",
-    date: "20 Jan 2024",
-  },
-  {
-    image: "/images/schoolDistrict/photo4.jpg",
-    title: "Practical education",
-    date: "20 Jan 2024",
-  },
-  {
-    image: "/images/schoolDistrict/photo5.jpg",
-    title: "Practical education",
-    date: "20 Jan 2024",
-  },
-  {
-    image: "/images/schoolDistrict/photo6.jpg",
-    title: "Practical education",
-    date: "20 Jan 2024",
-  },
-]);
-
-const videoList = ref([
-  {
-    image: "/images/schoolDistrict/video1.jpg",
-    title: "Practical education",
-    date: "20 Jan 2024",
-  },
-  {
-    image: "/images/schoolDistrict/video2.jpg",
-    title: "Practical education",
-    date: "20 Jan 2024",
-  },
-  {
-    image: "/images/schoolDistrict/video3.jpg",
-    title: "Practical education",
-    date: "20 Jan 2024",
-  },
-  {
-    image: "/images/schoolDistrict/video4.jpg",
-    title: "Practical education",
-    date: "20 Jan 2024",
-  },
-  {
-    image: "/images/schoolDistrict/video5.jpg",
-    title: "Practical education",
-    date: "20 Jan 2024",
-  },
-  {
-    image: "/images/schoolDistrict/video6.jpg",
-    title: "Practical education",
-    date: "20 Jan 2024",
   },
 ]);
 
@@ -187,7 +68,6 @@ const searchedJob= ref<string>('');
           >
             <!-- Tabs -->
             <div class="flex flex-col">
-              <!-- Loop through tabs -->
               <button
                   v-for="(tab, index) in tabs"
                   :key="index"
@@ -368,24 +248,16 @@ const searchedJob= ref<string>('');
                           ? "About School District"
                           : activeTab === 1
                               ? "List of Jobs"
-                              : activeTab === 2
-                                  ? "List of Schools"
-                                          : activeTab === 3
-                                              ? "Location"
-                                              : ""
+                                    : activeTab === 3
+                                        ? "Location" : ""
                     }}
                   </p>
                   <p
                       class="text-slate-600 text-base font-normal leading-normal"
                   >
                     {{
-                      activeTab === 0
-                          ? "Read out the information about patlo alto unified school."
-                          : activeTab === 1
-                              ? "Have a look to the list of Jobs."
-                              : activeTab === 2
-                                  ? "Have a look to the list of schools."
-                                          : ""
+                      activeTab === 0 ? "Read out the information about patlo alto unified school."
+                          : activeTab === 1 ? "Have a look to the list of Jobs." : ""
                     }}
                   </p>
                 </div>
@@ -420,8 +292,8 @@ const searchedJob= ref<string>('');
                 :searched-keyword="searchedJob"
               />
             </div>
-            <ListSchools :data="schoolList" v-if="activeTab === 2" />
 
+<!--            <ListSchools :data="schoolList" v-if="activeTab === 2" />-->
 <!--            <PhotoCard :data="photoList" v-if="activeTab === 3" />-->
 <!--            <VideoCard v-if="activeTab === 4" :data="videoList" />-->
 
